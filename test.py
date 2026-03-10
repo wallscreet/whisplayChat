3a6b39c2abf1149476d0936e90e7a62f41913d58
wallscreet <wallscreet@gmail.com>
2026-03-10 16:58:12 -0500

    add primary run loop

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from Driver.WhisPlay import WhisPlayBoard
 from utils import load_jpg_as_rgb565
+from time import sleep
 
 
 board = WhisPlayBoard()
@@ -26,3 +27,14 @@ try:
     print(f"Image {os.path.basename(image_filepath)} loaded and displayed.")
 except Exception as e:
     print(f"Failed to load image from {image_filepath}: {e}")
+
+try:
+    print("Loaded and waiting.. Press Ctrl+c to exit")
+    while True:
+        sleep(0.1)
+
+except KeyboardInterrupt:
+    print("Exiting test...")
+
+finally:
+    board.cleanup()
